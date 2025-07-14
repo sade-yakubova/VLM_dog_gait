97c5ef7968a6f4e3ce85fb55ae74dc09afb801e0
sade-yakubova <sade.yakubova@gmail.com>
2025-07-14 06:44:36 +0200

    Update constants.py

--- Analysis Dog Bot - Base and Lora 0.3/constants.py
+++ Analysis Dog Bot - Base and Lora 0.3/constants.py
@@ -3,6 +3,6 @@ USERNAME = "s2425823"
 PASSWORD = "***'
 REMOTE_DIR = f"/home/{USERNAME}/"  # Директория на кластере
 
-TOKEN = '7809930449:AAFWFE29tEhccuHtnUVZUwbBxGee32DZ1M4' # телеграм токен
+TOKEN = '***' # телеграм токен
 ADMINS = ['***']
 DEVELOPER_CHAT_ID = '***' # кому отправляется сообщение с ошибками в телеграм боте
